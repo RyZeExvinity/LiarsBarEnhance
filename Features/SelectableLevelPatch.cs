69c25e2dca212db7f2cf07f31d92c799de31f8b5
dogdie233 <dogdie233@163.com>
2024-10-23 09:16:22 +0800

    fix compile error

--- Features/SelectableLevelPatch.cs
+++ Features/SelectableLevelPatch.cs
@@ -5,13 +5,13 @@ using LiarsBarEnhance.Components;
 namespace LiarsBarEnhance.Features;
 
 [HarmonyPatch]
-public class XP
+public class SelectableLevelPatch
 {
     [HarmonyPatch(typeof(Statsui), "Start")]
     [HarmonyPostfix]
     public static void StartPostfix(Statsui __instance)
     {
         __instance.gameObject.AddComponent<SelectableLevelController>();
-        Plugin.Logger.LogDebug($"[{typeof(XP)}] Added {nameof(SelectableLevelController)} component to {__instance.gameObject.name}");
+        Plugin.Logger.LogDebug($"[{typeof(SelectableLevelPatch)}] Added {nameof(SelectableLevelController)} component to {__instance.gameObject.name}");
     }
 }
